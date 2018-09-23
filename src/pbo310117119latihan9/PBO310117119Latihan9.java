511c03910ca045887d3a8b1c3c300cf760ae4088
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:06:35 +0700

    first commit

---
+++ src/pbo310117119latihan9/PBO310117119Latihan9.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan9;
+
+/**
+ *
+ * @author  
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3/PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Sebuah Teks
+ */
+public class PBO310117119Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil"+ newline + "Char:" + tab + "berisi " + c);             
+    }
+    
+}
